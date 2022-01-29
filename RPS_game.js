405dc12d7eaa3cf1cfa30c04c4a4d1ca5ba492f4
Michael <mpphelps@gmail.com>
2022-01-29 13:57:48 -0500

    Added UI to RPS game

--- RPS_game.js
+++ RPS_game.js
@@ -2,6 +2,7 @@
 let RPS = ["Rock", "Paper", "Scissors"]; //string array of the possible options for a player to pick from
 let playerScore = 0; //stores the score of the player
 let computerScore = 0; //stores the score of the computer
+let tieCount = 0; //stores the count of ties
 let roundNumber = 1; //stores what round the game is on
 let gameRounds = 5; //defines the number of rounds in a game
 let roundOutcome; //stores the outcome of the round;
@@ -10,6 +11,7 @@ function computerPlay() {
     //calculates a random number from 0 to 2, and selects an option from the RPS string array
     let randomNumber = Math.floor(Math.random()*3);
     console.log("computerChoice: ", RPS[randomNumber]);
+    computerChoice_element.textContent = RPS[randomNumber];
     return RPS[randomNumber];
 }
 
@@ -21,6 +23,7 @@ function gameRound(playerSelection, computerSelection) {
         case "RockRock":
         case "PaperPaper":
         case "PaperPaper":
+            tieCount+=1;
             return "It's a tie";
         case "RockPaper":
         case "PaperScissors":
@@ -54,41 +57,82 @@ function gameRound(playerSelection, computerSelection) {
     
  }
 
- function playGame(){
-     //keep running the game until out of rounds
-     while(roundNumber<=gameRounds) {
-        //modify playerChoice so that first letter is capitalized and all remaining chars are lowercase
-        let playerChoice = prompt("Rock, Paper, or Scissors?");
-        playerChoice = playerChoice.toLowerCase();
-        playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1);
-        console.log("playerChoice: ", playerChoice);
-        //verify playerChoice is an available option otherwise recall prompt.
-        if (!(playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissors")) {
-            alert("That is not a valid option!");
-            continue;
-        }
+function playGame(playerChoice){
+    //modify playerChoice so that first letter is capitalized and all remaining chars are lowercase
+    //let playerChoice = prompt("Rock, Paper, or Scissors?");
+    playerChoice = playerChoice.toLowerCase();
+    playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1);
+    console.log("playerChoice: ", playerChoice);
+    //verify playerChoice is an available option otherwise recall prompt.
+    if (!(playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissors")) {
+        alert("That is not a valid option!");
+    }
+
+    if(roundNumber<=gameRounds) {
         roundOutcome = gameRound(playerChoice,computerPlay());
         console.log("Round ", roundNumber,": ", roundOutcome);
         console.log("Player Score: ", playerScore, "Computer Score: ", computerScore)
+    
+        computerScore_element.textContent = computerScore;
+        playerScore_element.textContent = playerScore;
+        tieCount_element.textContent = tieCount;
+        roundNumber_element.textContent = roundNumber;
+        
+        // if game rounds = 5 then check who is winner
+        console.log("roundNumber: ", roundNumber, "gameRounds: ", gameRounds);
+        if (roundNumber === gameRounds) {
+            if(computerScore > playerScore) {
+                gameResult_element.textContent = "The Computer Won!";
+            } else if (playerScore > computerScore) {
+                gameResult_element.textContent = "You Won!";
+            } else {
+                gameResult_element.textContent = "Its a tie!";
+            }
+        }
         roundNumber+=1;
-     }
-     if (computerScore>playerScore) {
-        resetGame();
-        return "You Lose the Game!";
-     }
-     if (playerScore>computerScore) {
-        resetGame();
-         return "You Win the Game!";
-     }
-     if (computerScore==playerScore) {
-        resetGame();
-         return "Game Tied!";
-     }
-     
- }
+    }
 
- function resetGame() {
-     roundNumber = 1;
-     computerScore = 0;
-     playerScore = 0;
- }
+}
+
+function resetGame() {
+    roundNumber = 1;
+    computerScore = 0;
+    playerScore = 0;
+    tieCount = 0;
+    gameResult_element.textContent = "Game Reset";
+    computerScore_element.textContent = computerScore;
+    playerScore_element.textContent = playerScore;
+    tieCount_element.textContent = tieCount;
+    roundNumber_element.textContent = roundNumber;
+    computerChoice_element.textContent = "";
+}
+
+
+const btnRock = document.querySelector('#btnRock');
+const btnPaper = document.querySelector('#btnPaper');
+const btnScissors = document.querySelector('#btnScissors');
+const btnResetGame = document.querySelector('#btnResetGame');
+
+btnRock.addEventListener('click', () => {
+    playGame(RPS[0]);
+});
+btnPaper.addEventListener('click', () => {
+    playGame(RPS[1]);
+});
+btnScissors.addEventListener('click', () => {
+    playGame(RPS[2]);
+});
+btnResetGame.addEventListener('click', () => {
+    resetGame();
+    
+});
+
+const computerScore_element = document.querySelector('#computerScore');
+const playerScore_element = document.querySelector('#playerScore');
+const roundNumber_element = document.querySelector('#roundNumber');
+const tieCount_element = document.querySelector('#tieCount');
+const gameResult_element = document.querySelector('#gameResult');
+const computerChoice_element = document.querySelector('#computerChoice');
+
+
+
